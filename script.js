5af99ddedc3183741797f6cabf3b50bac721e5f4
salimM-code <mustafasalim000@gmail.com>
2022-01-25 13:47:49 -0500

    added more helper functions

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 const generateTarget = () => Math.floor(Math.random() * 10);
+
+const compareGuesses = (humanGuess, computerGuess, secretNum) => {
+  const human = Math.abs(secretNum - humanGuess);
+  const computer = Math.abs(secretNum - computerGuess);
+
+  if(human === computer) return true;
+  if(human < computer) return true;
+  if(computer < human) return false;
+
+}
+
+const updateScore = winner => {
+  if(winner === 'human') {
+    humanScore += 1;
+  }
+
+  if(winner === 'computer'){
+    computerScore += 1;
+  }
+}
+
+const advanceRound = () => currentRoundNumber += 1;
+
